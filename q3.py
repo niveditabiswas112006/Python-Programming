f0df2a8dc9dcfb1eb33abcf4a30c3583cc3b2e0a
Nivedita (Nivi) Biswas <nivedita10012006@gmail.com>
2025-01-15 21:06:10 +0530

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,12 @@
+# 3. Arithmetic Operations
+# Prompt for two numbers. 
+# Compute and display the sum, difference, product, division, and modulus of those two numbers.
+
+num1=float(input("Enter the 1st num: "))
+num2=float(input("enter the 2nd num: "))
+
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num2 - num1}")
+print(f"Product: {num1 * num2}")
+print(f"Division: {num2 / num1}")
+print(f"Modulus: {num1 % num2}")
