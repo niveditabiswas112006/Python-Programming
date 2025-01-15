d81fe33dde9ff1c05f88d3acc922a95188bfe59b
Nivedita (Nivi) Biswas <nivedita10012006@gmail.com>
2025-01-15 21:24:58 +0530

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,8 @@
+# 7. Check Vowel or Consonant
+# Input a letter from the user and check if it’s a vowel or a consonant.
+
+letter=input("Enter a Letter: ")
+if letter in "a e i o u"
+   print(f"{letter} is a vowel")
+else:
+   print(f"{letter} is a consonant")
