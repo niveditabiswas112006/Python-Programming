2d485b24a47c29d3b06bea4c37fcf245c09c1ed2
Nivedita (Nivi) Biswas <nivedita10012006@gmail.com>
2025-01-15 21:00:26 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,10 @@
+# 2. Variables and Data Types
+# Prompt the user for their name and age. Print them in a sentence (e.g., "Your name is ... and your age is ...").
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: ")
+print(f"Your name is {name} and your age is {age}.")
+
+# name=input("Enter your name: ")
+# age=(input("Enter your age: ")
+# print(f"Your name is {name} and your age is {age}.")
