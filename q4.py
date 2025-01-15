c09fe9c252aa79ee0064454692ecf6d68a67f9fd
Nivedita (Nivi) Biswas <nivedita10012006@gmail.com>
2025-01-15 21:11:00 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,6 @@
+# 4. Convert Celsius to Fahrenheit
+# Take a temperature in Celsius and convert it to Fahrenheit
+
+celsius=float(input("Enter temperature of celsius: ")
+fahrenheit = (celsius * 9/5) + 32
+print(f"Temperature of Fahrenheit: {fehrenheit}")
